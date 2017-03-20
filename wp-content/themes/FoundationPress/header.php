a28152414ecdd5a276c98ba8773334ba021a12ec
Sean Ward <scward0@gmail.com>
2017-03-20 11:58:03 -0600

    stickey nav

--- wp-content/themes/FoundationPress/header.php
+++ wp-content/themes/FoundationPress/header.php
@@ -48,11 +48,11 @@
 		</nav>
 	</header>
 
-	<div class="social-nav" style="position: fixed; left: 0; top: 30%; z-index: 1000;">
-		<ul class="menu vertical" style="width: 100px; background: black; padding: 0;">
-			<li><a href="#"><img src="http://198.58.102.147/BridgeCommunications/wp-content/uploads/2017/03/facebook.png" style="z-index: 10000; width: 100px;"></a></li>
-			<li><a href="#"><img src="http://198.58.102.147/BridgeCommunications/wp-content/uploads/2017/03/stellar.png" style="z-index: 10000; width: 100px;"></a></li>
-			<li><a href="#"><img src="http://198.58.102.147/BridgeCommunications/wp-content/uploads/2017/03/youtube.png" style="z-index: 10000; width: 100px;"></a></li>
+	<div class="social-nav" style="position: fixed; left: 0; top: 30%; z-index: 1000; padding: 0;">
+		<ul class="menu vertical" style="width: 70px; background: black; padding: 0;">
+			<li><a href="#" style="padding: 0;"><img src="http://198.58.102.147/BridgeCommunications/wp-content/uploads/2017/03/facebook.png" style="z-index: 10000; width: 70px;"></a></li>
+			<li><a href="#" style="padding: 0;"><img src="http://198.58.102.147/BridgeCommunications/wp-content/uploads/2017/03/stellar.png" style="z-index: 10000; width: 70px;"></a></li>
+			<li><a href="#" style="padding: 0;"><img src="http://198.58.102.147/BridgeCommunications/wp-content/uploads/2017/03/youtube.png" style="z-index: 10000; width: 70px;"></a></li>
 		</ul>
 	</div>
 
