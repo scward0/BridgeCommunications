b32459102e703c0c53b05643b56be2f1e214a69d
Sean Ward <scward0@gmail.com>
2017-03-20 11:48:26 -0600

    social nav

--- wp-content/themes/FoundationPress/header.php
+++ wp-content/themes/FoundationPress/header.php
@@ -49,7 +49,7 @@
 	</header>
 
 	<div class="social-nav" style="position: fixed; left: 0; top: 30%; z-index: 1000;">
-		<ul class="menu vertical" style="width: 70px;">
+		<ul class="menu vertical" style="width: 70px; backgroud: black; padding: 0;">
 			<li><a href="#"><img src="http://198.58.102.147/BridgeCommunications/wp-content/uploads/2017/03/facebook.png" style="z-index: 10000; width: 70px;"></a></li>
 			<li><a href="#"><img src="http://198.58.102.147/BridgeCommunications/wp-content/uploads/2017/03/stellar.png" style="z-index: 10000; width: 70px;"></a></li>
 			<li><a href="#"><img src="http://198.58.102.147/BridgeCommunications/wp-content/uploads/2017/03/youtube.png" style="z-index: 10000; width: 70px;"></a></li>
