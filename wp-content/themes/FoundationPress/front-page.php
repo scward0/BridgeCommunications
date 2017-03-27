79a6edbc3388f1bd6dfbb97ae7e3761d018b523e
Sean Ward <scward0@gmail.com>
2017-03-27 16:41:38 -0600

    features services

--- wp-content/themes/FoundationPress/front-page.php
+++ wp-content/themes/FoundationPress/front-page.php
@@ -312,7 +312,7 @@
         </div>
       </div>
     </div>
-    <div class="row" style="padding-top: 100px;">
+    <div class="row" style="padding: 100px 5%;">
       <div class="columns small-12">
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
