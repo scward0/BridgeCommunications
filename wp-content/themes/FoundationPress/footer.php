d243c4f7705ff5f5b818ea3b3a6491b814e22a1b
Sean Ward <scward0@gmail.com>
2017-03-27 16:38:22 -0600

    dropdowns

--- wp-content/themes/FoundationPress/footer.php
+++ wp-content/themes/FoundationPress/footer.php
@@ -11,11 +11,6 @@
 	</div>
 </section>
 
-<!-- <section>
-	<img src="/BridgeCommunications/wp-content/uploads/2017/03/footer_example.png" width="100%" alt="">
-</section> -->
-
-
 <script type="text/javascript" src="/BridgeCommunications/wp-content/themes/FoundationPress/assets/javascript/slick/slick.min.js"></script>
 <script type="text/javascript" src="/BridgeCommunications/wp-content/themes/FoundationPress/assets/javascript/fullPage/jquery.fullPage.min.js"></script>
 <script type="text/javascript">
