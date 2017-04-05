79edea4d806b7333bfa10119b14f649b4892c64e
Sean Ward <scward0@gmail.com>
2017-04-05 16:09:12 -0600

    appcontents

--- wp-content/themes/FoundationPress/front-page.php
+++ wp-content/themes/FoundationPress/front-page.php
@@ -254,7 +254,7 @@
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       </div>
     </div>
-    <div class="dropdowns row small-up-1 medium-up-5 large-up-5" style="padding: 100px 0;">
+    <div class="dropdowns row small-up-1 medium-up-5 large-up-5" style="padding-bottom: 100px;">
       <div class="column">
         <ul class="vertical menu text-center" data-accordion-menu>
           <li style="text-align: left; margin-top: 10px;">
