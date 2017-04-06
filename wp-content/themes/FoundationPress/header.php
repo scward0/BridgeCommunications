cd1dcbcf6e031163752cecbfc5276cda4ecdfa0b
Sean Ward <scward0@gmail.com>
2017-04-06 10:26:48 -0600

    svg

--- wp-content/themes/FoundationPress/header.php
+++ wp-content/themes/FoundationPress/header.php
@@ -76,9 +76,9 @@
 
 	<div class="social-nav hide-for-small-only" style="position: fixed; left: 0; top: 30%; z-index: 1000; padding: 0;">
 		<ul class="menu vertical" style="width: 50px; background: black; padding: 0;">
-			<li><a href="#" style="padding: 10px 0;"><span data-tooltip aria-haspopup="true" class="has-tip right" data-disable-hover="false" tabindex="3" title="FACEBOOK" style="border-bottom: none;"><img class="style-svg" src="/BridgeCommunications/wp-content/uploads/2017/04/facebook.svg" style="z-index: 10000;" width="50px"></span></a></li>
-			<li><a href="#" style="padding: 10px 0;"><span data-tooltip aria-haspopup="true" class="has-tip right" data-disable-hover="false" tabindex="3" title="STELLAR" style="border-bottom: none;"><img class="style-svg" src="/BridgeCommunications/wp-content/uploads/2017/04/steller.svg" style="z-index: 10000;" width="50px"></span></a></li>
-			<li><a href="#" style="padding: 10px 0;"><span data-tooltip aria-haspopup="true" class="has-tip right" data-disable-hover="false" tabindex="3" title="YOUTUBE" style="border-bottom: none;"><img class="style-svg" src="/BridgeCommunications/wp-content/uploads/2017/04/youtube.svg" style="z-index: 10000;" width="50px"></span></a></li>
+			<li><a href="#" style="padding: 10px 0;"><span data-tooltip aria-haspopup="true" class="has-tip right" data-disable-hover="false" tabindex="3" title="FACEBOOK" style="border-bottom: none;"><img class="style-svg" src="/BridgeCommunications/wp-content/uploads/2017/04/facebook.svg" style="z-index: 10000;" width="25px"></span></a></li>
+			<li><a href="#" style="padding: 10px 0;"><span data-tooltip aria-haspopup="true" class="has-tip right" data-disable-hover="false" tabindex="3" title="STELLAR" style="border-bottom: none;"><img class="style-svg" src="/BridgeCommunications/wp-content/uploads/2017/04/steller.svg" style="z-index: 10000;" width="25px"></span></a></li>
+			<li><a href="#" style="padding: 10px 0;"><span data-tooltip aria-haspopup="true" class="has-tip right" data-disable-hover="false" tabindex="3" title="YOUTUBE" style="border-bottom: none;"><img class="style-svg" src="/BridgeCommunications/wp-content/uploads/2017/04/youtube.svg" style="z-index: 10000;" width="25px"></span></a></li>
 		</ul>
 	</div>
 
