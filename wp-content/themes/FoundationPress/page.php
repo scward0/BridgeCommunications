4aa5bc8464fc116e56547489e3c446ed53775dfe
Sean Ward <scward0@gmail.com>
2017-04-10 12:10:51 -0600

    page

--- wp-content/themes/FoundationPress/page.php
+++ wp-content/themes/FoundationPress/page.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
  <?php while ( have_posts() ) : the_post(); ?>
-  <div class="container">
+  <div class="container" style="padding: 200px 10%;">
     <div class="row">
       <h1><?php the_title(); ?></h1>
       <p><?php the_content(); ?></p>
