b666613785a7b800cbdc61c9adb3597c2aa17cf0
Sean Ward <scward0@gmail.com>
2017-04-10 10:21:13 -0600

    page

--- wp-content/themes/FoundationPress/front-page.php
+++ wp-content/themes/FoundationPress/front-page.php
@@ -248,11 +248,11 @@
         </div>
       </div>
     </div>
-    <div class="row" style="padding: 100px 10%;">
+    <!-- <div class="row" style="padding: 100px 10%;">
       <div class="columns small-12">
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       </div>
-    </div>
+    </div> -->
     <div class="dropdowns row small-up-1 medium-up-5 large-up-5" style="padding-bottom: 100px;">
       <div class="column">
         <ul class="vertical menu text-center" data-accordion-menu>
