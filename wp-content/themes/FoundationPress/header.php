4ca6ce11b985d79030cee135dbddf6d503a0e83a
Sean Ward <scward0@gmail.com>
2017-04-10 12:16:16 -0600

    social links

--- wp-content/themes/FoundationPress/header.php
+++ wp-content/themes/FoundationPress/header.php
@@ -66,9 +66,9 @@
 		</ul>
 		<div class="menu-centered">
 			<ul class="menu">
-				<li><a href="#"><img src="/BridgeCommunications/wp-content/uploads/2017/04/facebook.svg" class="style-svg style-svg-2" style="z-index: 10000;"></a></li>
-				<li><a href="#"><img src="/BridgeCommunications/wp-content/uploads/2017/04/stellar2.svg" class="style-svg style-svg-2" style="z-index: 10000;"></a></li>
-				<li><a href="#"><img src="/BridgeCommunications/wp-content/uploads/2017/04/youtube.svg" class="style-svg style-svg-2" style="z-index: 10000;"></a></li>
+				<li><a href="https://www.facebook.com/BRIDGE-Communication-Application-464631923700943/"><img src="/BridgeCommunications/wp-content/uploads/2017/04/facebook.svg" class="style-svg style-svg-2" style="z-index: 10000;"></a></li>
+				<li><a href="https://steller.co/search?q=bridgethegap&type=stories&order_by=top"><img src="/BridgeCommunications/wp-content/uploads/2017/04/stellar2.svg" class="style-svg style-svg-2" style="z-index: 10000;"></a></li>
+				<li><a href="https://www.youtube.com/channel/UCVIr5fmx0b2WTA1yluY-MkQ"><img src="/BridgeCommunications/wp-content/uploads/2017/04/youtube.svg" class="style-svg style-svg-2" style="z-index: 10000;"></a></li>
 			</ul>
 		</div>
 	</div>
@@ -76,9 +76,9 @@
 
 	<div class="social-nav hide-for-small-only" style="position: fixed; left: 0; top: 30%; z-index: 1000; padding: 0;">
 		<ul class="menu vertical" style="width: 50px; background: black; padding: 0;">
-			<li><a href="#" style="padding:0;"><span data-tooltip aria-haspopup="true" class="has-tip right" data-disable-hover="false" tabindex="3" title="FACEBOOK" style="border-bottom: none;"><img class="style-svg" src="/BridgeCommunications/wp-content/uploads/2017/04/facebook.svg" style="z-index: 10000;" width="25px"></span></a></li>
-			<li><a href="#" style="padding:0;"><span data-tooltip aria-haspopup="true" class="has-tip right" data-disable-hover="false" tabindex="3" title="STELLAR" style="border-bottom: none;"><img class="style-svg" src="/BridgeCommunications/wp-content/uploads/2017/04/stellar2.svg" style="z-index: 10000;" width="25px"></span></a></li>
-			<li><a href="#" style="padding:0;"><span data-tooltip aria-haspopup="true" class="has-tip right" data-disable-hover="false" tabindex="3" title="YOUTUBE" style="border-bottom: none;"><img class="style-svg" src="/BridgeCommunications/wp-content/uploads/2017/04/youtube.svg" style="z-index: 10000;" width="25px"></span></a></li>
+			<li><a href="https://www.facebook.com/BRIDGE-Communication-Application-464631923700943/" style="padding:0;"><span data-tooltip aria-haspopup="true" class="has-tip right" data-disable-hover="false" tabindex="3" title="FACEBOOK" style="border-bottom: none;"><img class="style-svg" src="/BridgeCommunications/wp-content/uploads/2017/04/facebook.svg" style="z-index: 10000;" width="25px"></span></a></li>
+			<li><a href="https://steller.co/search?q=bridgethegap&type=stories&order_by=top" style="padding:0;"><span data-tooltip aria-haspopup="true" class="has-tip right" data-disable-hover="false" tabindex="3" title="STELLAR" style="border-bottom: none;"><img class="style-svg" src="/BridgeCommunications/wp-content/uploads/2017/04/stellar2.svg" style="z-index: 10000;" width="25px"></span></a></li>
+			<li><a href="https://www.youtube.com/channel/UCVIr5fmx0b2WTA1yluY-MkQ" style="padding:0;"><span data-tooltip aria-haspopup="true" class="has-tip right" data-disable-hover="false" tabindex="3" title="YOUTUBE" style="border-bottom: none;"><img class="style-svg" src="/BridgeCommunications/wp-content/uploads/2017/04/youtube.svg" style="z-index: 10000;" width="25px"></span></a></li>
 		</ul>
 	</div>
 
