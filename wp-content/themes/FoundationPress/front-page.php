c7f9cd5d1c5b272d484737c6dbbbb46a17937178
Sean Ward <scward0@gmail.com>
2017-04-10 13:24:09 -0600

    test slider

--- wp-content/themes/FoundationPress/front-page.php
+++ wp-content/themes/FoundationPress/front-page.php
@@ -248,11 +248,6 @@
         </div>
       </div>
     </div>
-    <!-- <div class="row" style="padding: 100px 10%;">
-      <div class="columns small-12">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      </div>
-    </div> -->
     <div class="dropdowns row small-up-1 medium-up-1 large-up-5" style="padding: 100px 10%;">
       <div class="column">
         <ul class="vertical menu text-center" data-accordion-menu>
@@ -278,27 +273,21 @@
                   <li>why</li>
                 </ul>
               </li>
-
-
               <li><a href="#">Feedback</a>
                 <ul class="menu vertical nested">
                   <li>positive</li>
                   <li>negative</li>
                 </ul>
               </li>
-
               <li>
                 Answers
               </li>
-
               <li>
                 My Grids
               </li>
-
               <li>
                 Words
               </li>
-
               <li>
                 Problem
               </li>
